531da3aedb0e941e36507027a2aeb1701d90d644
andywildridge <andy.wildridge@gmail.com>
2021-05-04 18:16:48 +0100

    added more tone experiments

---
+++ granular.js
@@ -0,0 +1,46 @@
+
+		// the player
+		const player = new Tone.GrainPlayer({
+			url: "https://tonejs.github.io/audio/berklee/arpeggio3crazy.mp3",
+			loop: true,
+			grainSize: 0.1,
+			overlap: 0.05,
+		}).toDestination();
+
+		ui({
+			parent: document.querySelector("#content"),
+			tone: player,
+		});
+
+		// bind the interface
+		document.querySelector("tone-play-toggle").addEventListener("start", () => player.start());
+		document.querySelector("tone-play-toggle").addEventListener("stop", () => player.stop());
+
+	/// PLAYER START
+
+		// the player
+		const player = new Tone.Player({
+			url: "https://tonejs.github.io/audio/loop/FWDL.mp3",
+			loop: true,
+			loopStart: 0.5,
+			loopEnd: 0.7,
+		}).toDestination();
+
+		ui({
+			tone: player,
+			parent: document.querySelector("#content")
+		});
+
+		// bind the interface
+		document.querySelector("tone-play-toggle").addEventListener("start", () => player.start());
+		document.querySelector("tone-play-toggle").addEventListener("stop", () => player.stop());
+	
+
+        //clock
+
+        // the callback will be invoked approximately once a second
+// and will print the time exactly once a second apart.
+const clock = new Tone.Clock(time => {
+	console.log(time);
+}, 1);
+clock.start();
